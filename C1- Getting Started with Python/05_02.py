f0ffe47436f36666a95c5e3511332f2e09065ad5
phuongle1197 <ltphuong1197@gmail.com>
2020-08-22 18:44:57 +0300

    Assignment 5.2

---
+++ C1- Getting Started with Python/05_02.py
@@ -0,0 +1,27 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+
+    try:
+        num = int(num)
+    except:
+        print("Invalid input")
+        continue
+
+    if smallest is None:
+        smallest = num
+    elif num < smallest:
+        smallest = num
+
+    if largest is None:
+        largest = num
+    elif num > largest:
+        largest = num
+
+
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
